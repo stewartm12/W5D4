a79a36e3257d16101f3c60d3779e1bfb9802e5df
alejandro <alejandroweil13@gmail.com>
2021-01-28 16:20:15 -0500

    Saved file

--- URL_shortener/URLShortener/app/models/shortened_url.rb
+++ URL_shortener/URLShortener/app/models/shortened_url.rb
@@ -1,4 +1,15 @@
 class ShortenedUrl < ApplicationRecord 
     validates :long_url, :short_url, :user_id, presence: true, uniqueness: true
+    def random_code
+
+    end
+end
+
+
+class SecureRandom
+
+    def self.urlsafe_base64
+        
+    end
 
 end
