47ca5d019d1b1b0871b28cce4be087ebd19d9168
alejandro <alejandroweil13@gmail.com>
2021-01-28 17:21:29 -0500

    associated shortened_url and user classes

--- URL_shortener/URLShortener/app/models/shortened_url.rb
+++ URL_shortener/URLShortener/app/models/shortened_url.rb
@@ -2,12 +2,24 @@ class ShortenedUrl < ApplicationRecord
     validates :long_url, :user_id, presence: true
     validates :short_url, presence: true, uniqueness: true 
 
+    belongs_to :submitter,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id   
+    
+    
     def self.random_code
        self.short_url = loop do 
         random_url =  SecureRandom.urlsafe_base64(22, true)
         break random_url unless self.ShortenedUrl.exists?(short_url: random_url)
        end
     end
+    def create!(user, long_url)
+        user.id  
+    end
+
+
+    
 end
 
 
