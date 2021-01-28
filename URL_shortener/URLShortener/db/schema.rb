2fff89cf80272167187e37b91061e0f226925416
stewart morales <stewartmorales129@gmail.com>
2021-01-28 15:42:09 -0500

    Create ShortenedUrls table

--- URL_shortener/URLShortener/db/schema.rb
+++ URL_shortener/URLShortener/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210128200108) do
+ActiveRecord::Schema.define(version: 20210128203441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string "long_url"
+    t.string "short_url"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
