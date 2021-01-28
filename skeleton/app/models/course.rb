ef2e22a5f9d4693b6f6058afc2d80df0be8a5819
stewart morales <stewartmorales129@gmail.com>
2021-01-28 14:00:32 -0500

    Add first Associations

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  has_many :users,
+  
 end
--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+  class_name: 'Enrollment',
+  foreign_key: :student_id,
+  primary_key: :id 
 end
